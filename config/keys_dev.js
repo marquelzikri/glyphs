beeaf36277fa32e26246d900759aa63cda18b9a8
Muhammad Zikri Cirrus Marquel Bismar <33832889+marquelzikri@users.noreply.github.com>
2018-07-24 12:30:02 +0700

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://zikrimarquel:vazrielMarquel8@ds051543.mlab.com:51543/glyphs-api',
+  mongoURI: 'mongodb://admin:admin123@ds051543.mlab.com:51543/glyphs-api',
   secretOrKey: 'TIA-Glyphs-secret-key_1998003'
 };
